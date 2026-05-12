bafd9e1ac16ddbd38993c1262122145cd73802fc
Valentin Lukyanets <valikluks95@gmail.com>
2026-05-12 15:23:56 +0300

    Home assignment 4: declared structures with input and output data

--- homework_04/src/main.cpp
+++ homework_04/src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <fstream>
 
+// Struct that contains robot position and current timestamp
+struct RobotPosition {
+    long timestamp_ms;
+    double x;
+    double y;
+    double theta;
+};
+
+// Struct that contains odometry reading and timestamp
+struct RobotOdometryReading {
+    long timestamp_ms;
+    long fl_ticks;
+    long fr_ticks;
+    long bl_ticks;
+    long br_ticks;
+};
 
 int main(int argc, char** argv) {
     // The program expects exactly one argument: a path to telemetry samples.
@@ -16,6 +32,10 @@ int main(int argc, char** argv) {
         return 2;
     }
 
+    // Initialize with zeroes robot position
+    RobotPosition robot_position{};
+    // Use pair of consecutive odometry readings for wheel odometry math
+    RobotOdometryReading prev_reading, current_reading;
 
     return 0;
 }
