c5a509e4c24403182f9235c71065417f3d44457e
Valentin Lukyanets <valikluks95@gmail.com>
2026-05-12 15:21:31 +0300

    Home assignment 4: open input file and handle error

--- homework_04/src/main.cpp
+++ homework_04/src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+
 
 int main(int argc, char** argv) {
     // The program expects exactly one argument: a path to telemetry samples.
@@ -7,17 +9,13 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    // TODO: implement wheel odometry for a 4-wheel differential-drive UGV.
-    //
-    // Model parameters:
-    //   ticks_per_revolution = 1024
-    //   wheel_radius_m       = 0.3
-    //   wheelbase_m          = 1.0
-    //
-    // Input: a text file with 5 whitespace-separated values per line:
-    //         timestamp_ms fl_ticks fr_ticks bl_ticks br_ticks
-    // Output: a table on stdout, starting from the second sample:
-    //         timestamp_ms x y theta
+    // The program should open input file by path from the argument
+    std::ifstream ifs{argv[1]};
+    if (!ifs.is_open()) {
+        std::cerr << "cannot open file " << argv[1] << "\n";
+        return 2;
+    }
+
 
     return 0;
 }
