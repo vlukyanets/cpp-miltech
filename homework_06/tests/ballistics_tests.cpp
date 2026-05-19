e8f8afc13e7fc2888b8a2d79ba9292ffb16897f0
Valentin Lukyanets <valikluks95@gmail.com>
2026-05-19 12:28:39 +0300

    Added tests for ballistics task

---
+++ homework_06/tests/ballistics_tests.cpp
@@ -0,0 +1,54 @@
+#include "ballistics.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(Ballistics, ComputesKnownDropPoint)
+{
+  const BallisticsInput input{
+    .position{100.0, 100.0},
+    .target{200.0, 200.0},
+    .altitude = 100.0,
+    .attack_speed = 10.0,
+    .acceleration_path = 10.0,
+    .ammo_name = "VOG-17",
+  };
+
+  const BallisticsSolution solution = calculate_ballistics(input);
+
+  EXPECT_EQ(solution.result, SolutionResult::Ok);
+  EXPECT_FALSE(solution.intermediate.has_value());
+  EXPECT_NEAR(solution.fire.x, 173.759, 0.01);
+  EXPECT_NEAR(solution.fire.y, 173.759, 0.01);
+}
+
+TEST(Ballistics, NegativeAltitude)
+{
+  const BallisticsInput input{
+    .position{100.0, 100.0},
+    .target{200.0, 200.0},
+    .altitude = -100.0,
+    .attack_speed = 10.0,
+    .acceleration_path = 10.0,
+    .ammo_name = "VOG-17",
+  };
+
+  const BallisticsSolution solution = calculate_ballistics(input);
+
+  EXPECT_EQ(solution.result, SolutionResult::NegativeAltitude);
+}
+
+TEST(Ballistics, UnknownAmmoName)
+{
+  const BallisticsInput input{
+    .position{100.0, 100.0},
+    .target{200.0, 200.0},
+    .altitude = -100.0,
+    .attack_speed = 10.0,
+    .acceleration_path = 10.0,
+    .ammo_name = "HEHEHE",
+  };
+
+  const BallisticsSolution solution = calculate_ballistics(input);
+
+  EXPECT_EQ(solution.result, SolutionResult::NegativeAltitude);
+}
